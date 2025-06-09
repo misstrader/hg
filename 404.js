8da084cb0b80c91a10249fd746c7028176f0f51d
misstrader <reihane1234r1@gmail.com>
2025-06-09 13:40:44 +0330

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1 style={{ color: "red", textAlign: "center", paddingTop: "2rem" }}>404 - صفحه پیدا نشد</h1>;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Miss Trader Full Site
+
+Next.js website for trading education with dark/green theme, multiple pages, and bilingual support.
